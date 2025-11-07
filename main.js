ffd724b72627207f77d11f27f741c7790b7cb171
Tomikimi <tomikimiogoli@yahoo.com>
2025-11-07 11:18:04 +0100

    Creation of Bible Message Components

--- main.js
+++ main.js
@@ -1,16 +1,22 @@
-const bibleQuotes = [
+const bibleQuotes1 = [
   "Philipians 4:13 \n I can do all things through Christ who strengthens me.\n",
   "Jeremiah 29:11 \n For i Know the plans i have for you, declares the Lord, plans to prosper you and not to harm, plans to give you a hope and a future.\n",
   "Isaiah 41:10 \n Do not fear, for i am with you; do not be dismayed, for i am your God. I will strengthen you and help you.\n",
   "Romans 8:28 \n And we Know that in all things God works for the good of those who love him.\n",
   "Psalm 46:1 \n God is our refuge and strength, an ever-present help in trouble.\n",
   "Proverbs 3:5-6 \n Trust in the Lord with all your heart and lean not on your own understanding; in all your ways acknowledge Him, and He will make your paths straight.\n",
+];
+
+const bibleQuotes2 = [
   "Joshua 1:9 \n Be strong and courageous. Do not be afraid; do not be discouraged, for the Lord your God will be with you wherever you go.\n",
   "Psalm 37:4 \n Delight yourself in the Lord, and He will give you the desires of your heart.\n",
   "2 Corinthians 5:7 \n For we live by faith, not by sight.\n",
   "Matthew 19:26 \n With man this is impossible, but with God all things are possible.\n",
   "Isaiah 40:31 \n But those who hope in the Lord will renew their strength. They will soar on wings like eagles.\n",
   "Psalm 23:1 \n The Lord is my shepherd; I shall not want.\n",
+];
+
+const bibleQuotes3 = [
   "Romans 12:12 \n Be joyful in hope, patient in affliction, faithful in prayer.\n",
   "Deuteronomy 31:6 \n Be strong and of good courage; do not fear nor be afraid of them, for the Lord your God goes with you.\n",
   "Psalm 121:1-2 \n I lift up my eyes to the hills—where does my help come from? My help comes from the Lord, the Maker of heaven and earth.\n",
@@ -45,6 +51,12 @@ function generateRandomIndex() {
   return index;
 }
 
+function getRandomBibleQuote(param) {
+  let quote = bibleQuotes[param];
+
+  return quote;
+}
+
 // Get the FullName of the User of the App
 rl.question(
   "\n --Bible Quote Generator-- \n\n Hi, What is your Name?",
@@ -54,3 +66,6 @@ rl.question(
     rl.close();
   }
 );
+
+let index = generateRandomIndex();
+console.log(getRandomBibleQuote(index));
